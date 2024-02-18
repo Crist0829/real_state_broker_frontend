70c2fa2a2db202e12599ccc5ca2d3d50d618111b
LuisJimenez19 <luisjian948@gmail.com>
2024-02-18 12:54:14 -0300

    arreglo propertyCalification

--- src/components/properties/PropertyCalification.tsx
+++ src/components/properties/PropertyCalification.tsx
@@ -1,87 +1,94 @@
+import { PropertyCalificationType, User } from "@/types";
+import { useEffect, useState } from "react";
+import { toast } from "sonner";
+import ReactStars from "react-rating-star-with-type";
+import { useAuthenticate } from "@/store/useAuthenticate";
+import PropertyMyCalification from "./PropertyMyCalification";
 
-import axios from '@/lib/axiosConfig'
-import { PropertyCalificationType, User } from '@/types'
-import { useEffect, useState } from 'react'
-import { toast } from 'sonner'
-import ReactStars from 'react-rating-star-with-type'
-import { useAuthenticate } from '@/store/useAuthenticate'
+const PropertyCalification = ({
+  califications,
+  propertyId,
+  setCalification,
+}: {
+  califications: PropertyCalificationType[];
+  propertyId: number;
+  setCalification?: any;
+}) => {
+  const [generalCalification, setGeneralCalification] = useState<number>(0);
 
-const PropertyCalification = ({califications, propertyId, setCalification} : 
-    {califications : PropertyCalificationType[], propertyId : number, setCalification? : any}) => {
-    const [generalCalification, setGeneralCalification] = useState<number>(0)
-    const [myCalification, setMyCalification] = useState<PropertyCalificationType | null >(null)
-    const [showMyCalification, setShowMyCalification] = useState<boolean>(false)
-    const [ user, setUser] = useState<User | null>(null)
-    const isAuthenticated = useAuthenticate((state) => state.isAuthenticated)
-    const userAuthenticated = useAuthenticate((state) => state.user)
+  const [myCalification, setMyCalification] =
+    useState<PropertyCalificationType | null>(null);
 
-    
+  const [showMyCalification, setShowMyCalification] = useState<boolean>(false);
+  const [user, setUser] = useState<User | null>(null);
+  const isAuthenticated = useAuthenticate((state) => state.isAuthenticated);
+  const userAuthenticated = useAuthenticate((state) => state.user);
 
-    useEffect(() => {
-        const checkAuthenticated = () => {
-            try {
-                if (isAuthenticated) {
-                    setUser(userAuthenticated)
-                    setShowMyCalification(true)
-                    if (califications.length > 0) {
-                        const myCalificationR = califications.find(calification => calification.user_id === user?.id)
-                        if(myCalificationR) setMyCalification(myCalificationR)
-                    }
-                }
-               
-            } catch (error : any) {
-                toast.error('Error al obtener datos del usuario:', error);
-            }
+  useEffect(() => {
+    const checkAuthenticated = () => {
+      try {
+        if (isAuthenticated) {
+          setUser(userAuthenticated);
+          setShowMyCalification(true);
+          if (califications.length > 0) {
+            const myCalificationR = califications.find(
+              (calification) => calification.user_id === user?.id
+            );
+
+            if (myCalificationR) setMyCalification(myCalificationR);
+          }
+        } else {
+          setShowMyCalification(false);
         }
-        checkAuthenticated();
-    }, [isAuthenticated]);
+      } catch (error: any) {
+        toast.error("Error al obtener datos del usuario:", error);
+      }
+    };
 
-    useEffect(() => {
-         // Saco la calificación general
-         if (califications.length > 0) {
-            const nC = califications.length
-            const tC = califications.reduce((total, calification) => total + calification.calification, 0)
-            const gC = Math.round(tC / nC)
-            setGeneralCalification(gC)
-        }
-    },[setCalification])
+    checkAuthenticated();
+  }, [califications, isAuthenticated, user, userAuthenticated]);
 
-    const changeHandle = async (e : any) => {
-        const res = await axios.post('/property/addCalification/' + propertyId, { calification : e})
-        setCalification(true)
-        setMyCalification(res.data)
-        toast.success("Calificación agregada", {
-            description: "Has calificado esta propiedad",
-          });
+  useEffect(() => {
+    // Saco la calificación general
+    if (califications.length > 0) {
+      const nC = califications.length;
+      const tC = califications.reduce(
+        (total, calification) => total + calification.calification,
+        0
+      );
+      const gC = Math.round(tC / nC);
+      setGeneralCalification(gC);
     }
+  }, [setCalification]);
 
-    return(
-        <div className='flex gap-8 my-10 items-center justify-center'>
+  /* calification temporal */
 
-            <div className={`flex flex-col items-center justify-center ${generalCalification == 0 && 'mt-5'}`}>
-              <p className='text-sm my-1'>Calificación general:</p>
-              <ReactStars  
-                value={generalCalification}  
-                isEdit={true}  
-                activeColors={[ "red", "orange", "#FFCE00", "#9177FF","#8568FC",]} />
+  return (
+    <div className="flex gap-8 my-5 items-center justify-center ">
+      <div
+        className={`flex flex-col items-center justify-center ${
+          generalCalification == 0 && "mt-5"
+        }`}
+      >
+        <p className="text-sm my-1">Calificación general:</p>
+        <ReactStars
+          value={generalCalification}
+          isEdit={true}
+          activeColors={["red", "orange", "#FFCE00", "#9177FF", "#8568FC"]}
+        />
+      </div>
 
-            </div>
-
-            {
-            showMyCalification && 
-            <div className={`flex flex-col items-center justify-center ${generalCalification == 0 && 'mt-5'}`}>
-                <p className='text-sm my-1'>Tu calificación:</p>
-                <ReactStars 
-                    onChange={(e)=> changeHandle(e)} 
-                    value={myCalification?.calification}  
-                    isEdit={true}  
-                    activeColors={[ "red", "orange", "#FFCE00", "#9177FF","#8568FC",]}/>
-            </div>
-            }
+      {showMyCalification && (
+        <PropertyMyCalification
+          generalCalification={generalCalification}
+          myCalification={myCalification}
+          propertyId={propertyId}
+          setCalification={setCalification}
+          setMyCalification={setMyCalification}
+        />
+      )}
     </div>
+  );
+};
 
-       
-    )
-}
-
-export default PropertyCalification 
+export default PropertyCalification;
